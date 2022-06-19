ae02b65e56e126df7d6dc9ef5e4a56b63e69f5ae
brodie96 <101592243+brodie96@users.noreply.github.com>
2022-06-20 00:27:35 +0200

    added forecast

--- javascript/script.js
+++ javascript/script.js
@@ -36,12 +36,29 @@ if (minutes < 10) {
   minutes = `0${minutes}`;
 }
 if (hour < 10) {
-  hour = `0${hour};`;
+  hour = `0${hour}`;
 }
 
 currentDay.innerHTML = `${day}  ${date} ${month}  ${hour}:${minutes}`;
 console.log(`${day}  ${date} ${month}  ${hour}:${minutes}`);
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 //main
+
+function getForecast(geocoords) {
+  console.log(geocoords);
+  let apiKey = "0bc997255f5a3bef3c5d47209c8da7d5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${geocoords.lat}&lon=${geocoords.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(showWeatherForecast);
+}
+
 function showData(request) {
   let temperature = Math.round(request.data.main.temp);
   console.log(request);
@@ -62,6 +79,8 @@ function showData(request) {
     `http://openweathermap.org/img/wn/${request.data.weather[0].icon}@2x.png`
   );
   celsiusTemperature = request.data.main.temp;
+
+  getForecast(request.data.coord);
 }
 
 function getTemperature(city) {
@@ -83,10 +102,10 @@ function getLocation(event) {
   navigator.geolocation.getCurrentPosition(getCurrentLocation);
 }
 
-function getCurrentLocation(geocords) {
+function getCurrentLocation(geocoords) {
   let apiKey = "0bc997255f5a3bef3c5d47209c8da7d5";
-  let lon = Math.round(geocords.coords.longitude);
-  let lat = Math.round(geocords.coords.latitude);
+  let lon = Math.round(geocoords.coords.longitude);
+  let lat = Math.round(geocoords.coords.latitude);
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&&units=metric`;
   axios.get(apiUrl).then(showData);
 }
@@ -122,45 +141,70 @@ fahrenheitTemp.addEventListener("click", degreesFahrenheit);
 let celsiusTemp = document.querySelector("#celsius-temp");
 celsiusTemp.addEventListener("click", degreesCelsius);
 
-function showWeatherForecast() {
+function showWeatherForecast(response) {
+  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let weatherForecast = document.querySelector("#weather-forecast");
   let forecastHTML = `<div class="row" id= "weather-forecast">`;
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
                   
-                     <div class="col-12" onmouseover="showForecastTemp()">
+                     <div id="forecast-${index}" class="col-12">
                      <div class="weather-forecast-date">
-                      ${day}<br /></div>
-                         <img src="http://openweathermap.org/img/wn/10d@2x.png" width=30%/ class="forecast-icons"> <br/>
+                      ${formatDay(forecastDay.dt)}<br /></div>
+                         <img id="pic-${index}" style="display:'';" src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" width=30%/ class="forecast-icons"> <br/>
+                          <span style="display: none;" class="span1" id="max-${index}"> ${Math.round(
+          forecastDay.temp.max
+        )}º|</span><span style="display: none;" class="span2" id="min-${index}">${Math.round(
+          forecastDay.temp.min
+        )}º</span>
+                 
                  
                     </div>`;
+    }
   });
+
   forecastHTML = forecastHTML + `</div>`;
 
   weatherForecast.innerHTML = forecastHTML;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      let forecast = document.querySelector(`#forecast-${index}`);
+      forecast.addEventListener("mouseover", showForecastTemp);
+      forecast.addEventListener("mouseout", showForecastImage);
+    }
+  });
 }
-showWeatherForecast();
 
 function showForecastTemp() {
-  let weatherForecast = document.querySelector("#weather-forecast");
-  let forecastHTML = `<div class="row" id= "weather-forecast">`;
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-                  
-                     <div class="col-12" onmouseleave="showWeatherForecast()">
-                     <div class="weather-forecast-date">
-                      ${day}<br /></div>
-                        <span id="max-forecast-temp"> 18º</span>|<span id="min-forecast-temp">12º</span>
-                 
-                    </div>`;
-  });
-  forecastHTML = forecastHTML + `</div>`;
+  let days = ["0", "1", "2", "3", "4"];
 
-  weatherForecast.innerHTML = forecastHTML;
+  days.forEach(function (day) {
+    let max = document.querySelector(`#max-${day}`);
+    max.style.display = "inline";
+    let min = document.querySelector(`#min-${day}`);
+    min.style.display = "initial";
+    let icon = document.querySelector(`#pic-${day}`);
+    icon.style.display = "none";
+  });
+}
+
+function showForecastImage() {
+  let days = ["0", "1", "2", "3", "4"];
+
+  days.forEach(function (day) {
+    let max = document.querySelector(`#max-${day}`);
+    max.style.display = "none";
+    let min = document.querySelector(`#min-${day}`);
+    min.style.display = "none";
+    let icon = document.querySelector(`#pic-${day}`);
+    icon.style.display = "initial";
+  });
 }
